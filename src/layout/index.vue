fe59f15dab709aebd1723c7db6dbe38553af98e0
甘壮 <1933024994@qq.com>
2022-06-28 21:20:46 +0800

    feat(layout布局): layout布局 and 页面布局

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -9,5 +9,48 @@
     </div>
   </div>
 </template>
-<script setup></script>
-<style lang="scss" scoped></style>
+
+<script setup>
+import Sidebar from './Sidebar'
+import Navbar from './Navbar'
+import AppMain from './AppMain'
+</script>
+
+<style lang="scss" scoped>
+.app-wrapper {
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  .sidebar-container {
+    width: 210px;
+    height: 100%;
+    background-color: #304156;
+    overflow-y: auto;
+    position: fixed;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    -ms-overflow-style: none; /* IE 10+ */
+    scrollbar-width: none; /* Firefox */
+  }
+
+  .main-container {
+    width: calc(100% - 210px);
+    height: 100%;
+    margin-left: 210px;
+    position: relative;
+
+    .fixed-header {
+      position: fixed;
+      top: 0;
+      right: 0;
+      z-index: 9;
+      width: calc(100% - 210px);
+    }
+  }
+}
+::-webkit-scrollbar {
+  display: none; /* Chrome Safari */
+}
+</style>
